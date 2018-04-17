97a17782c0d9865d47ddc90f1913d2e126c04164
denbnddb <denbnddb_pc_4520_5630@yahoo.co.jp>
2018-04-17 14:46:41 +0900

    コードの整理

--- test.php
+++ test.php
@@ -1,9 +1,6 @@
 <?php
-//$filename='https://creator.zohopublic.com/legolandjapan/waiting-time/view-embed/htmlview/tqRfOTn4AjD9PPYhkP6S2Fw75XqKgJwz8OmtCxkHZCY07Ss0AG6bT8BzAhwbhPyUt6PAAy7S5DPZCJRnSFbAte44CzUVztbaSzpw?';
-//$filename='https://www.legoland.jp/plan-your-visit/ride-wait-times/';
 $filename="https://creator.zohopublic.com/legolandjapan/waiting-time/view-embed/htmlview/tqRfOTn4AjD9PPYhkP6S2Fw75XqKgJwz8OmtCxkHZCY07Ss0AG6bT8BzAhwbhPyUt6PAAy7S5DPZCJRnSFbAte44CzUVztbaSzpw?";
 $source = file_get_contents($filename);
-//$source_array = explode( PHP_EOL, $source );
 echo $source;
 
 
